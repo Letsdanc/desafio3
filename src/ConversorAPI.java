985c3b80d7f75f6521f26f047783899704b30891
Jose Arturo <152576275+Letsdanc@users.noreply.github.com>
2024-11-20 11:57:01 -0400

    ConversorAPI.java

--- src/ConversorAPI.java
+++ src/ConversorAPI.java
@@ -13,14 +13,12 @@ public class ConversorAPI {
     public double obtenerTasa(String desdeMoneda, String aMoneda) {
         double tasa = 0.0;
         try {
-            // URL de la API con tu API Key y las monedas solicitadas
             String urlString = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + desdeMoneda;
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
 
-            // Leer la respuesta de la API
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuilder response = new StringBuilder();
             String line;
@@ -28,10 +26,9 @@ public class ConversorAPI {
                 response.append(line);
             }
 
-            // Extraer la tasa de cambio del JSON
             String responseString = response.toString();
 
-            // Buscar la tasa de cambio para la moneda de destino en la respuesta
+       
             int index = responseString.indexOf("\"" + aMoneda + "\":");
             if (index != -1) {
                 String rateStr = responseString.substring(index + aMoneda.length() + 3, responseString.indexOf(",", index));
